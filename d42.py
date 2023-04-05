40fd2afcec3b1f8c1c084c402ef1e3373eed44e7
Sayed Bakhtiar Junaid <82361101+sbjunaid@users.noreply.github.com>
2023-04-05 23:50:53 +0530

    Add files via upload

---
+++ d42.py
@@ -0,0 +1,15 @@
+#Matrix Diagonal Sum
+
+class Solution:
+    def diagonalSum(self, mat: List[List[int]]) -> int:
+        n = len(mat)
+        ans = 0
+
+        for i in range(n):
+            ans += mat[i][i]
+            ans += mat[n - 1 - i][i]
+        
+        if n % 2 != 0:
+             ans -= mat[n // 2][n // 2]
+        
+        return ans
