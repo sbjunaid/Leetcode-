95c30691ef8c3946239b151c98bd9d07ba3b7a20
Sayed Bakhtiar Junaid <82361101+sbjunaid@users.noreply.github.com>
2023-04-12 00:57:19 +0530

    Add files via upload

---
+++ d101.py
@@ -0,0 +1,11 @@
+#Minimum Cost to Move Chips to The Same Position
+
+
+class Solution:
+    def minCostToMoveChips(self, position: List[int]) -> int:
+        even=0
+        for i in position:
+            if i%2==0:
+                even+=1
+            
+        return min(even,len(position)-even)
