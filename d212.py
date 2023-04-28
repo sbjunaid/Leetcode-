f19af37906345dba8b96b4a14cbaed7803d4f7ae
Sayed Bakhtiar Junaid <82361101+sbjunaid@users.noreply.github.com>
2023-04-29 02:01:59 +0530

    Add files via upload

---
+++ d212.py
@@ -0,0 +1,12 @@
+# Intersection of Two Arrays
+
+class Solution:
+    def intersection(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """  
+        set1 = set(nums1)
+        set2 = set(nums2)
+        return list(set2 & set1)
