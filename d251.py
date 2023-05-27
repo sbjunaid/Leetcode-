8559484cdf0aefcdb2674da917210aeafe6c3d99
Sayed Bakhtiar Junaid <82361101+sbjunaid@users.noreply.github.com>
2023-05-27 10:46:00 +0530

    Add files via upload

---
+++ d251.py
@@ -0,0 +1,22 @@
+#Search in Rotated Sorted Array
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l = 0
+        r = len(nums) - 1
+        while l <= r:
+            mid = l + (r-l) // 2
+            if target == nums[mid]:
+                return mid
+            if nums[mid] >= nums[l]:
+                if target < nums[l] or target > nums[mid]:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+            else:
+                if target > nums[r] or target < nums[mid]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+        return -1
+        
